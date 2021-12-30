88b93d87d0417d9dc4ebf06ffb141bdb37bb0792
hec7orci7o <hectortoraltrabajos@gmail.com>
2021-12-30 15:22:04 +0100

    ...

--- scrapper.py
+++ scrapper.py
@@ -1,4 +1,4 @@
-import time, re, random, json, Levenshtein, sys
+import time, re, random, json, Levenshtein
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
